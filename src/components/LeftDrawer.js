436786b9e08dbdbc1d047d802ea3a6017913497f
martitasea <marta.pulido.asin@gmail.com>
2022-09-05 18:31:57 +0200

    Estilo LeftDrawer

--- src/components/LeftDrawer.js
+++ src/components/LeftDrawer.js
@@ -1,6 +1,7 @@
 import React, {useState, useCallback} from 'react';
 import PropTypes from 'prop-types';
 //MUI
+import Box from '@mui/material/Box';
 import Drawer from '@mui/material/Drawer';
 import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
@@ -10,37 +11,35 @@ import ExitToApp from '@mui/icons-material/ExitToApp';
 //MENORCA-VT
 import ResponsiveHeader from './ResponsiveHeader';
 import LogoBlanco from '../../static/img/LogoBlanco';
-//STYLES
-import makeStyles from '@mui/styles/makeStyles';
-
+//UTILS
 import {setLoggedIn} from '../actions';
 import {useDispatch} from 'react-redux';
-
-const useStyles = makeStyles((theme) => ({
-  toolbar: theme.mixins.toolbar,
-  drawerTitle: {
-    padding: theme.spacing(2.5, 0, 0, 2),
-    fontWeight: 'bold',
-    marginBottom: 5
-  },
-  drawerContent: {
-    padding: theme.spacing(0, 4, 2.5, 2),
-    display: 'flex',
-    flexWrap: 'wrap',
-  },
-  dragger: {
-    display: 'flex',
-    alignItems: 'center',
-    width: '30px',
-    height: '100%',
-    cursor: 'ew-resize',
-    padding: '40px 0 0',
-    position: 'absolute',
-    right: 0,
-    zIndex: 100,
-    color: theme.palette.grey[500],
-  }
-}));
+//STYLES
+const toolbar = {
+  toolbar: theme => theme.mixins.toolbar,
+};
+const dragger = {
+  display: 'flex',
+  alignItems: 'center',
+  width: '30px',
+  height: '100%',
+  cursor: 'ew-resize',
+  padding: '40px 0 0',
+  position: 'absolute',
+  right: 0,
+  zIndex: 100,
+  color: 'grey.500',
+};
+const drawerTitle = {
+  padding: theme => theme.spacing(2.5, 0, 0, 2),
+  fontWeight: 'bold',
+  marginTop: 7,
+};
+const drawerContent = {
+  padding: theme => theme.spacing(0, 4, 2.5, 2),
+  display: 'flex',
+  flexWrap: 'wrap',
+};
 
 const LeftDrawer = ({defaultDrawerWidth, children, onDrawerWidthChange}) => {
   const dispatch = useDispatch();
@@ -48,7 +47,6 @@ const LeftDrawer = ({defaultDrawerWidth, children, onDrawerWidthChange}) => {
   const minDrawerWidth = defaultDrawerWidth;
   const maxDrawerWidth = defaultDrawerWidth*4;
   const [drawerWidth, setDrawerWidth] = useState(defaultDrawerWidth);
-  const classes = useStyles({defaultDrawerWidth, drawerWidth});
 
   const handleMouseDown = () => {
     document.addEventListener('mouseup', handleMouseUp, true);
@@ -75,19 +73,19 @@ const LeftDrawer = ({defaultDrawerWidth, children, onDrawerWidthChange}) => {
       variant='permanent'
       PaperProps={{style: {width: drawerWidth}}}
     >
-      <div className={classes.toolbar}/>
+      <Box sx={toolbar}/>
       <ResponsiveHeader logo={<LogoBlanco/>} width={drawerWidth}>
         <IconButton onClick={handleLogout} size="large">
           <ExitToApp/>
         </IconButton>
       </ResponsiveHeader>
-      <div onMouseDown={e => handleMouseDown(e)} className={classes.dragger}>
+      <Box onMouseDown={e => handleMouseDown(e)} sx={dragger}>
         <ArrowForwardIosIcon/>
-      </div>
-      <Typography className={classes.drawerTitle} variant='h6'>Visor d&#039;expedients</Typography>
-      <div className={classes.drawerContent}>
+      </Box>
+      <Typography sx={drawerTitle} variant='h6'>Visor d&#039;expedients</Typography>
+      <Box sx={drawerContent}>
         {children}
-      </div>
+      </Box>
     </Drawer>
   );
 };
