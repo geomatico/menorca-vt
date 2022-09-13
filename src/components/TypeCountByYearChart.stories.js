d172b865696c412a86b907fb87a96aa2d3d99475
Oscar Fonts <oscar.fonts@geomati.co>
2022-09-13 13:01:29 +0200

    Update & restore storybook

--- src/components/TypeCountByYearChart.stories.js
+++ src/components/TypeCountByYearChart.stories.js
@@ -14,6 +14,6 @@ const Template = (args) => <TypeCountByYearChart {...args} />;
 
 export const Default = Template.bind({});
 Default.args = {
-  categories: config.categories,
+  categories: config.consellCategories,
   data: fixture.data
 };
