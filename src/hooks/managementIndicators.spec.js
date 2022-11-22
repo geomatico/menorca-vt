319387b69af505f70cd9b76a99dbbeca4ab376a5
Alex Molto <alex.molto@geomatico.es>
2022-11-22 18:16:12 +0100

    Refactor end_date y start_date por date

--- src/hooks/managementIndicators.spec.js
+++ src/hooks/managementIndicators.spec.js
@@ -9,20 +9,13 @@ describe('Data indicators', () => {
 
     const getTypeCountByStartDate = () => {
       const filteredFixtures = fixture
-        .map(fix => ({
-          tipus: fix.tipus,
-          data_inici: new Date(fix.data_inici).getFullYear()
-        }))
+        .map(fix => ({tipus: fix.tipus, data_inici: new Date(fix.data_inici).getFullYear()}))
         .filter(el => el.data_inici);
 
       return Object.values(filteredFixtures.reduce((r, e) => {
         let k = `${e.tipus}|${e.data_inici}`;
         if (e.data_inici) {
-          if (!r[k]) r[k] = {
-            type: e.tipus,
-            start_date: e.data_inici,
-            value: 1
-          };
+          if (!r[k]) r[k] = {type: e.tipus, date: e.data_inici, value: 1};
           else r[k].value += 1;
         }
         return r;
@@ -36,42 +29,42 @@ describe('Data indicators', () => {
     const expectedState = [
       {
         type: 'EX073',
-        start_date: 2017,
+        date: 2017,
         value: 1
       },
       {
         type: 'EX068',
-        start_date: 2018,
+        date: 2018,
         value: 1
       },
       {
         type: 'CED',
-        start_date: 2021,
+        date: 2021,
         value: 4
       },
       {
         type: 'CED',
-        start_date: 2018,
+        date: 2018,
         value: 6
       },
       {
         type: 'CED',
-        start_date: 2020,
+        date: 2020,
         value: 1
       },
       {
         type: 'CED',
-        start_date: 2019,
+        date: 2019,
         value: 3
       },
       {
         type: 'CED',
-        start_date: 2022,
+        date: 2022,
         value: 6
       },
       {
         type: 'CED',
-        start_date: 2017,
+        date: 2017,
         value: 1
       }
     ];
@@ -83,18 +76,12 @@ describe('Data indicators', () => {
     const givenState = fixture;
 
     const getTypeCountByEndDate = () => {
-      const newFixtures = fixture.map(fix => ({
-        tipus: fix.tipus,
-        data_fi: new Date(fix.data_fi).getFullYear()
-      }));
+      const newFixtures = fixture.map(fix => ({tipus: fix.tipus, data_fi: new Date(fix.data_fi).getFullYear()}));
+
       return Object.values(newFixtures.reduce((r, e) => {
         let k = `${e.tipus}|${e.data_fi}`;
         if (e.tipus && e.data_fi) {
-          if (!r[k]) r[k] = {
-            type: e.tipus,
-            end_date: e.data_fi,
-            value: 1
-          };
+          if (!r[k]) r[k] = {type: e.tipus, date: e.data_fi, value: 1};
           else r[k].value += 1;
         }
         return r;
@@ -108,32 +95,32 @@ describe('Data indicators', () => {
     const expectedState = [
       {
         type: 'EX073',
-        end_date: 2018,
+        date: 2018,
         value: 1
       },
       {
         type: 'EX068',
-        end_date: 2018,
+        date: 2018,
         value: 1
       },
       {
         type: 'CED',
-        end_date: 2021,
+        date: 2021,
         value: 6
       },
       {
         type: 'CED',
-        end_date: 2018,
+        date: 2018,
         value: 5
       },
       {
         type: 'CED',
-        end_date: 2022,
+        date: 2022,
         value: 5
       },
       {
         type: 'CED',
-        end_date: 2019,
+        date: 2019,
         value: 4
       }
     ];
@@ -145,13 +132,10 @@ describe('Data indicators', () => {
     const givenState = fixture;
 
     const days_between = (date1, date2) => {
-
       // The number of milliseconds in one day
       const ONE_DAY = 1000 * 60 * 60 * 24;
-
       // Calculate the difference in milliseconds
       const differenceMs = Math.abs(new Date(date1) - new Date(date2));
-
       // Convert back to days and return
       return Math.round(differenceMs / ONE_DAY);
     };
@@ -159,21 +143,14 @@ describe('Data indicators', () => {
     const getAverageProcessingTimeByType = () => {
       // limpia las fixtures de datos corruptos
       const filteredFixtures = Object.values(fixture)
-        .map(fix => ({
-          type: fix.tipus,
-          daysDiff: days_between(fix.data_inici, fix.data_fi)
-        }))
+        .map(fix => ({type: fix.tipus, daysDiff: days_between(fix.data_inici, fix.data_fi)}))
         .filter(el => !!el.daysDiff);
 
       return Object.values(filteredFixtures
         .reduce((r, e) => {
           let k = `${e.type}`;
           if (e.type) {
-            if (!r[k]) r[k] = {
-              type: e.type,
-              value: e.daysDiff,
-              count: 1
-            };
+            if (!r[k]) r[k] = {type: e.type, value: e.daysDiff, count: 1};
             else {
               r[k].value += e.daysDiff;
               r[k].count += 1;
@@ -218,18 +195,11 @@ describe('Data indicators', () => {
     };
 
     const getStatusCountByYear = () => {
-      const newFixtures = fixture.map(fix => ({
-        status: fix.resolucio,
-        year: fix.any
-      }));
+      const newFixtures = fixture.map(fix => ({status: fix.resolucio, year: fix.any}));
       const res = Object.values(newFixtures.reduce((r, e) => {
         let k = `${e.status}|${e.year}`;
         if (e.status && e.year) {
-          if (!r[k]) r[k] = {
-            status: e.status,
-            year: e.year,
-            value: 1
-          };
+          if (!r[k]) r[k] = {status: e.status, year: e.year, value: 1};
           else r[k].value += 1;
         }
         return r;
