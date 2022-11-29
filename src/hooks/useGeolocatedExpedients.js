38bf95873cc6be104c81a7456bb70a4e8fbcead4
Alex Molto <alex.molto@geomatico.es>
2022-11-29 17:11:14 +0100

    clean

--- src/hooks/useGeolocatedExpedients.js
+++ src/hooks/useGeolocatedExpedients.js
@@ -8,16 +8,7 @@ const useGeolocatedExpedients = (visibleCategories, dateRange) => {
 
   const allVisibleCategoriesIds = Object.values(visibleCategories).flat();
 
-  //const altresTipusExpdients = Object.values(config.datasets).map(el => el.categories)[0].find(el => el.id === 'altres').values;
-  /*  console.log(333, altresTipusExpdients, '----');
-
-  /!*  const filteredExpedients = useMemo(()=> allExpedients?.valor?.filter(el => allVisibleCategoriesIds.includes(el.tipus)) );
-    }, [allExpedients?.valor])*!/*/
-
   return useMemo(() => allExpedients?.valor?.filter(el => {
-    /*    if(visibleCategories.consell.includes('Altres')) {
-            console.log('hola')
-          }*/
     return allVisibleCategoriesIds.includes(el.tipus) && el.exercici >= dateRange[0] && el.exercici <= dateRange[1];
   }),
   [visibleCategories, dateRange, allExpedients]);
